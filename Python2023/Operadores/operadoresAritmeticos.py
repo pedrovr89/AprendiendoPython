02d66e9f03c665271d5fcba3a924b294207a2a3c
pedrovr89 <pedrovr89@outlook.es>
2023-11-24 14:46:46 +0100

    Lecciones operadores

---
+++ Python2023/Operadores/operadoresAritmeticos.py
@@ -0,0 +1,17 @@
+#Operadores aritméticos (+,-,*,/,%,**,//)
+numero1 = 10
+numero2 = 5
+suma = numero1 + numero2
+print(suma)
+resta = numero1 - numero2
+print(resta)
+multiplicacion = numero1 * numero2
+print(multiplicacion)
+division = numero1 / numero2
+print(division)
+resto = numero1 % numero2
+print(resto)
+cociente = numero1 // numero2
+print(cociente)
+exponente = numero1 ** 3
+print(exponente)
